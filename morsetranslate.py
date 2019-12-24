7f7535f8bb73f7f55935e2747d371e55405c6c69
MEGATHERARPRO <57454184+MEGATHERARPRO@users.noreply.github.com>
2019-12-24 15:31:37 +0300

    Add files via upload

---
+++ morsetranslate.py
@@ -0,0 +1,74 @@
+b = int(1)
+while b == 1:
+	print('English:')
+	a = str(input())
+	if a == 'a':
+		print('.-')
+	elif a == 'b':
+		print('-...')
+	elif a == 'c':
+		print('-.-.')
+	elif a == 'd':
+		print('-..')
+	elif a == 'e':
+		print('.')
+	elif a == 'f':
+		print('..-.')
+	elif a == 'g':
+		print('--.')
+	elif a == 'h':
+		print('....')
+	elif a == 'i':
+		print('..')
+	elif a == 'j:
+		print('.---')
+	elif a == 'k':
+		print('-.-')
+	elif a == 'l':
+		print('.-..')
+	elif a == 'm':
+		print('--')
+	elif a == 'n:
+		print('-.')
+	elif a == 'o':
+		print('---')
+	elif a == 'p':
+		print('.--.')
+	elif a == 'q':
+		print('--.-')
+	elif a == 'r':
+		print('.-.')
+	elif a == 't':
+		print('-')
+	elif a == 'u':
+		print('..-')
+	elif a == 'v':
+		print('...-')
+	elif a == 'w':
+		print('.--')
+	elif a == 'x':
+		print('-..-')
+	elif a == 'y':
+		print('-.--')
+	elif a == 'z':
+		print('--..')
+	elif a == '1':
+		print('.----')
+	elif a == '2':
+		print('..---')
+	elif a == '3':
+		print('...--')
+	elif a == '4':
+		print('....-')
+	elif a == '5':
+		print('.....')
+	elif a == '6':
+		print('-....')
+	elif a == '7':
+		print('--...')
+	elif a == '8':
+		print('---..')
+	elif a == '9':
+		print('----.')
+	elif a == '0':
+		print('-----')
